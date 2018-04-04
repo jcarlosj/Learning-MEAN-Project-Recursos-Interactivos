26c42734bc3dc52d279e64dd9a37bf87ded973b2
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-04-04 18:16:15 -0500

    Realiza petición a URL, consume los datos a través de 'SliderService' visualiza en 'SliderComponent' por consola

--- src/app/components/slider.component.ts
+++ src/app/components/slider.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SliderService } from '../services/slider.service';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
     selector: 'app-slider',
@@ -7,8 +8,35 @@ import { SliderService } from '../services/slider.service';
     providers: [ SliderService ]
 })
 export class SliderComponent {
+    /* Propiedades (Atributos) */
+    public slideJson: string;
+
     /* Constructor */
     public constructor( private _SliderService: SliderService ) {
-        console .log( 'Servicio Slider: ', _SliderService .test() );
+        // console .log( 'Servicio Slider: ', _SliderService .test() );      // DEBUG
+
+        /* Hacemos un llamado al método que hace la petición a través del Servicio y usamos el 'subscribe'
+           para obtener el resultado a través de un 'CallBack' y poder imprimir el 'Observable' */
+        this ._SliderService .requestFile() .subscribe(
+            result => {
+                this .slideJson = result;  // Obtiene una cadena tipo JSON
+
+                console .log( 'Todo el archivo:', this .slideJson );
+
+                // Recorre cada item del 'Array' para obtener los datos de cada objeto contenido en el mismo
+                for( let i = 0; i < this .slideJson .length; i++ ) {
+                    console .group( `Datos del primer objeto ${ i }` );
+                    console .log( 'Título: ', this .slideJson[ i ][ 'titulo' ] );
+                    console .log( 'Descripción: ', this .slideJson[ i ][ 'descripcion' ] );
+                    console .log( 'Imagen: ', this .slideJson[ i ][ 'imagen' ] );
+                    console .groupEnd();
+                }
+
+            },
+            error => {
+                let message = <any> error;
+            }
+        );
+
     }
 }
--- src/app/services/slider.service.ts
+++ src/app/services/slider.service.ts
@@ -7,11 +7,24 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 
 export class SliderService {
-    /* DEBUG */
+    /* Porpiedades (Atributos) */
     public path_image = 'assets/images/slide/slide01.jpg';
+    public urlJsonFile: any;
 
-    /* Método */
+    /* Constructor */
+    public constructor( private _http: Http ) {
+        // Asigna la URL a la propiedad
+        this .urlJsonFile = 'http://tutorialesatualcance.com/slide.json';
+    }
+
+    /* Métodos */
+    public requestFile () {
+        // Realiza petición al módulo Http de Angular pasandole la URL donde se encuentra el archivo solicitado
+        return this ._http .get( this .urlJsonFile ) .map( result => result .json() );  // Mapea el resultado a través de un CallBack
+    }
+
     public test(): string {
         return this .path_image;
     }
+
 }
